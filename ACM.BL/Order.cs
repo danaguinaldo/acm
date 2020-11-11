0c330d80200990f3ba5a98d1a9cd4b9977767aa0
Danny Aguinaldo <dan.r.aguinaldo@gmail.com>
2020-11-11 15:21:40 -0600

    Created Order Class and Tests

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -13,14 +13,60 @@ namespace ACM.BL
                OrderDate = DateTime.Today;
           }
 
+          public Order(int orderNumber)
+          {
+               OrderDate = DateTime.Today;
+               OrderNumber = orderNumber;
+          }
+
+          public int OrderNumber { get; private set; }
+
           public DateTime OrderDate { get; set; }
+          
+          /// <summary>
+          /// Validate current order.
+          /// </summary>
+          /// <returns></returns>
           public bool Validate()
           {
                var isValid = true;
 
                if (OrderDate != DateTime.Today) isValid = false;
+               if (OrderNumber <= 0) isValid = false;
 
                return isValid;
+          }
+
+          /// <summary>
+          /// Retrieve defined order.
+          /// </summary>
+          /// <param name="orderNumber"></param>
+          /// <returns></returns>
+          public Order Retrieve(int orderNumber)
+          {
+               // Code to retrieve defined order
+
+               return new Order();
+          }
+
+          /// <summary>
+          /// Retrieve ALL orders for customer.
+          /// </summary>
+          /// <returns></returns>
+          public Order Retrieve()
+          {
+               // Code to retrieve ALL orders.
+
+               return new Order();
+          }
+
+          /// <summary>
+          /// Save current order.
+          /// </summary>
+          /// <returns></returns>
+          public bool Save()
+          {
+               return true;
           }
      }
 }
--- Tests/ACM.BLTest/OrderTest.cs
+++ Tests/ACM.BLTest/OrderTest.cs
@@ -11,9 +11,39 @@ namespace ACM.BLTest
           public void ValidateValid()
           {
                //-- Arrange
-               var order = new Order();
+               var order = new Order(1);
 
                var expected = true;
+               //-- Act
+               var actual = order.Validate();
+
+               //-- Assert
+               Assert.AreEqual(actual, expected);
+          }
+
+          [TestMethod]
+          public void OrderNumberNegativeNumber()
+          {
+               //-- Arrange
+               var order = new Order(-1);
+
+               var expected = false;
+
+               //-- Act
+               var actual = order.Validate();
+
+               //-- Assert
+               Assert.AreEqual(actual, expected);
+          }
+
+          [TestMethod]
+          public void OrderNumberMissing()
+          {
+               //-- Arrange
+               var order = new Order();
+
+               var expected = false;
+
                //-- Act
                var actual = order.Validate();
 
