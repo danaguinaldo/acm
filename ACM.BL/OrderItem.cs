71dfe076e93b62a5d8a4f46e6b7efcaf7f082caf
Danny Aguinaldo <dan.r.aguinaldo@gmail.com>
2020-11-11 15:28:19 -0600

    Added OrderItem class

---
+++ ACM.BL/OrderItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+     public class OrderItem
+     {
+          public Product OrderProduct { get; set; }
+          public int Quantity { get; set; }
+          public double PurchasePrice { get; set; }
+
+          /// <summary>
+          /// Validate OrderItem
+          /// </summary>
+          /// <returns></returns>
+          public bool Validate()
+          {
+               var isValid = true;
+
+               if (Quantity == 0) isValid = false;
+               if (OrderProduct == null) isValid = false;
+
+               return isValid;
+          }
+          
+          /// <summary>
+          /// Retrieve defined OrderItem
+          /// </summary>
+          /// <returns></returns>
+          public OrderItem Retrieve()
+          {
+               // Code to retrieve defined OrderItem
+
+               return new OrderItem();
+          }
+
+          /// <summary>
+          /// Saves Current OrderItem.
+          /// </summary>
+          /// <returns></returns>
+          public bool Save()
+          {
+               // Save current OrderItem 
+               
+               return true;
+          }
+     }
+}
