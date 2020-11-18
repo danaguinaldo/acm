2713d149eb0cb258d8d635f2af7dca78dd23a0c7
Danny Aguinaldo <dan.r.aguinaldo@gmail.com>
2020-11-18 09:46:25 -0600

    Create AddressRepository class.

---
+++ ACM.BL/AddressRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+     public class AddressRepository
+     {
+          /// <summary>
+          /// Retrieve one address.
+          /// </summary>
+          /// <param name="addressId"></param>
+          /// <returns></returns>
+          public Address Retrieve(int addressId)
+          {
+               // Create the instance of the Address class
+               // Pass inthe requested Id
+               Address address = new Address(addressId);
+
+               // Code that retrieves the defined address
+
+               // Temp hard code for return
+               if (addressId == 1)
+               {
+                    address.AddressType = 1;
+                    address.StreetLine1 = "Bag End";
+                    address.StreetLine2 = "Bagshot row";
+                    address.City = "Hobbiton";
+                    address.State = "Shire";
+                    address.Country = "Middle Earth";
+                    address.PostalCode = "144";
+               }
+
+               return address;
+          }
+
+          /// <summary>
+          /// Save current address.
+          /// </summary>
+          /// <returns></returns>
+          public bool Save()
+          {
+               return true;
+          }
+     }
+}
