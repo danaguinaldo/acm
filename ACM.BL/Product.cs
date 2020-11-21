7a7ec611750cd8e4ad8e0d594554f119d6963f1f
Danny Aguinaldo <dan.r.aguinaldo@gmail.com>
2020-11-21 11:53:08 -0600

    Changed StringHandler and InsertSpace into extension methods.

--- ACM.BL/Product.cs
+++ ACM.BL/Product.cs
@@ -24,7 +24,7 @@ namespace ACM.BL
           {
                get
                {
-                    return StringHandler.InsertSpaces(_productName);
+                    return _productName.InsertSpaces();
                }
                set
                {
