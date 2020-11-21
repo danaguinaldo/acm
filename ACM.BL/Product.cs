fc8f555b5d16661bf782761cde3f3a3c5abd4201
Danny Aguinaldo <dan.r.aguinaldo@gmail.com>
2020-11-21 11:38:16 -0600

    Add general StringHandler class and test class.

--- ACM.BL/Product.cs
+++ ACM.BL/Product.cs
@@ -18,7 +18,18 @@ namespace ACM.BL
                ProductId = productId;
           }
 
-          public string ProductName { get; set; }
+          public string _productName;
+          public string ProductName
+          {
+               get
+               {
+                    return _productName;
+               }
+               set
+               {
+                    _productName = value;
+               }
+          }
           public string ProductDescription { get; set; }
           public decimal? CurrentPrice { get; set; }
           public int ProductId { get; private set; }
---
+++ Tests/Acme.CommonTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Acme.CommonTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Acme.CommonTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("da5afcd2-fa21-4862-a55f-b0ed938db8ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
